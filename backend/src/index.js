2925314ddb0c4cfbd19490e50f6f0827ee549e6b
cesarvspr <vinicius_spr@hotmail.com>
2020-03-20 17:52:23 -0400

    updates, cant write on input

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
-const routes = require('./routes.js')
+const routes = require('./routes')
+const cors = require('cors')
 
 const app = express();
 mongoose.connect('mongodb+srv://cesao:adminzao@cluster0-prdnb.mongodb.net/week10?retryWrites=true&w=majority', 
@@ -9,7 +10,7 @@ mongoose.connect('mongodb+srv://cesao:adminzao@cluster0-prdnb.mongodb.net/week10
     },   
                  );
 
-
+app.use(cors());
 app.use(express.json())
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,42 +1,107 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+
 // componente : bloco isolado de HTML CSS e JS q nao interfere no restante da aplicacao 
 // estado: infromacoes mantidas pelo componente (lembrar imutabilidade)
 // propriedade:  informacoes que um componente pai passa para o componente filho
+
+
   
 
 function App() {
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithub_Username] = useState('');
+  const [techs, setTechs] = useState('');
+  
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude} = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+
+      },
+      (err) => {
+        console.log(err);
+      },  
+      {
+        timeout: 30000,
+      }
+    )
+  }, []
+  );
+
+
+  async function handdleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+  console.log(response.data);    
+  }
+
+
 
   return (
     <div id="app">
        <aside>
          <strong>Create account</strong>
          <form>
-          <div class= "input-block">
+          <div className= "input-block">
             <label htmlFor="github_username">GitHub user</label>
-            <input name="github_username" id="github_username" required />
+            <input 
+            name="github_username" 
+            id="github_username" 
+            required value={github_username}
+            onChage={e=>setGithub_Username(e.target.value)} 
+            />
           </div>
 
-          <div class= "input-block">
+          <div className= "input-block">
             <label htmlFor="techs">Techs</label>
-            <input name="techs" id="techs" required /> 
+            <input 
+            name="techs" 
+            id="techs" 
+            required value={techs}
+            onChage={e => setTechs(e.target.value)}
+            /> 
           </div>
 
 
             <div className="input-group">
-              <div class= "input-block">
+              <div className= "input-block">
                 <label htmlFor="latitude">latitude</label>
-                <input name="latitude" id="latitude" required /> 
+                <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required value={latitude}
+                onChage={e => setLatitude(e.target.value)} 
+                /> 
               </div>
 
-              <div class= "input-block">
+              <div className= "input-block">
                 <label htmlFor="longitude">longitude</label>
-                <input name="longitude" id="longitude" required /> 
+                <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required value={longitude} 
+                onChage={e => setLongitude(e.target.value)}
+                /> 
               </div>
             </div>
             <button type="submit">Save</button>

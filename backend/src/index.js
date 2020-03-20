3a1855c0e8729ebdf7d3b66bcd50d0a9de740a27
cesarvspr <vinicius_spr@hotmail.com>
2020-03-20 17:57:09 -0400

    update, cant write at input

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
-const routes = require('./routes')
-const cors = require('cors')
+const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://cesao:adminzao@cluster0-prdnb.mongodb.net/week10?retryWrites=true&w=majority', 
